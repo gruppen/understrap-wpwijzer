44fb97a9507ae216e39dea304b2aa4f116a4a5fd
Maik <gruppen@users.noreply.github.com>
2018-04-04 15:00:33 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,73 @@
+<?php
+function understrap_remove_scripts() {
+    wp_dequeue_style( 'understrap-styles' );
+    wp_deregister_style( 'understrap-styles' );
+
+    wp_dequeue_script( 'understrap-scripts' );
+    wp_deregister_script( 'understrap-scripts' );
+
+    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
+}
+add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+
+	// Get the theme data
+	$the_theme = wp_get_theme();
+    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
+    wp_enqueue_script( 'jquery');
+    wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
+    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+}
+
+
+/**
+ * Remove the migrate script from the list of jQuery dependencies.
+ *
+ * @since 1.0.0
+ *
+ * @param WP_Scripts $scripts WP_Scripts scripts object. Passed by reference.
+ */
+function maik_dequeue_jquery_migrate( $scripts ) {
+    if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
+        $jquery_dependencies = $scripts->registered['jquery']->deps;
+        $scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array( 'jquery-migrate' ) );
+    }
+}
+add_action( 'wp_default_scripts', 'maik_dequeue_jquery_migrate' );
+
+
+
+// no jqeury migrate
+function _remove_script_version( $src ){ 
+    $parts = explode( '?', $src );  
+    return $parts[0]; 
+} 
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 ); 
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+
+// No Emojii assets loaded
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+// Disable prefetching s.w.org for emojii svg's 
+add_filter( 'emoji_svg_url', '__return_false' );
+
+
+
+// disable XMLRPC
+add_filter('xmlrpc_enabled', '__return_false');
+
+
+remove_action ('wp_head', 'rsd_link');
+
+remove_action( 'wp_head', 'wlwmanifest_link');
+
+
+
+// remove RSS feed 
+remove_action( 'wp_head','feed_links', 2 );
+remove_action( 'wp_head','feed_links_extra', 3 );
